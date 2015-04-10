f0b643bb5b351da835c3be92d8785763a0d67bfa
BernhardKonrad <bernhard.konrad@gmail.com>
2015-04-10 13:39:46 -0700

    Adds a first, simple implementation of gambler's ruin.

--- gamblers_ruin.py
+++ gamblers_ruin.py
@@ -1,1 +1,16 @@
 # Write python script to simulate gambler's ruin
+
+from random import random
+
+moneyA = 10
+moneyB = moneyA
+
+while moneyA > 0 and moneyB > 0:
+    if random() < 0.5:
+        moneyA += 1
+        moneyB -= 1
+    else:
+        moneyA -= 1
+        moneyB += 1
+
+print(moneyA, moneyB)
