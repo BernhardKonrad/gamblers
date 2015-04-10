ff85cdc6406bfbc19a89b701c2b91c654d2b62f0
BernhardKonrad <bernhard.konrad@gmail.com>
2015-04-10 14:19:42 -0700

    Counts the number of games played.

--- gamblers_ruin.py
+++ gamblers_ruin.py
@@ -5,6 +5,7 @@ from random import random
 moneyA = 10
 moneyB = moneyA
 
+num_games = 0
 while moneyA > 0 and moneyB > 0:
     if random() < 0.5:
         moneyA += 1
@@ -12,5 +13,6 @@ while moneyA > 0 and moneyB > 0:
     else:
         moneyA -= 1
         moneyB += 1
+    num_games += 1
 
-print(moneyA, moneyB)
+print(moneyA, moneyB, num_games)
